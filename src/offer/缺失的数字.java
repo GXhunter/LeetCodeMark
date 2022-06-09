0864098a7694fc877a60e5225018480a18b6c1ec
wanggx <wangguoxiang@4399inc.com>
2022-06-09 17:38:59 +0800

    剑指offer

---
+++ src/offer/缺失的数字.java
@@ -0,0 +1,17 @@
+package offer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 缺失的数字 {
+    public int missingNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i <= nums.length; i++) {
+            set.add(i);
+        }
+        for (int num : nums) {
+            set.remove(num);
+        }
+        return (int) set.toArray()[0];
+    }
+}
