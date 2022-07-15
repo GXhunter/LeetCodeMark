ed09d953df3dd0af9b8255be542c5a056ec37506
wanggx <wangguoxiang@4399inc.com>
2022-07-15 18:29:09 +0800

    一个困难

--- src/tree/计算右侧小于当前元素的个数.java
+++ src/tree/计算右侧小于当前元素的个数.java
@@ -6,34 +6,24 @@ import java.util.stream.Collectors;
 
 /**
  * 基于归并排序
- * todo 重复元素bug
  * <a href="https://leetcode.cn/problems/count-of-smaller-numbers-after-self/">...</a>
  */
 public class 计算右侧小于当前元素的个数 {
-    Pair[] temp;
+    int[][] temp;
     int[] result;
 
-    private class Pair {
-        int val, id;
-        Pair(int val, int id) {
-            // 记录数组的元素值
-            this.val = val;
-            // 记录元素在数组中的原始索引
-            this.id = id;
-        }
-    }
     public List<Integer> countSmaller(int[] nums) {
-        temp = new Pair[nums.length];
+        temp = new int[nums.length][2];
         result = new int[nums.length];
-        Pair[] params = new Pair[nums.length];
+        int[][] params = new int[nums.length][2];
         for (int i = 0; i < nums.length; i++) {
-            params[i] = new Pair(nums[i],i);
+            params[i] = new int[]{nums[i], i};
         }
         sortRange(params, 0, nums.length);
         return Arrays.stream(result).boxed().collect(Collectors.toList());
     }
 
-    private void sortRange(final Pair[] nums, int from, int to) {
+    private void sortRange(final int[][] nums, int from, int to) {
         if (from == to - 1) {
             return;
         }
@@ -47,11 +37,11 @@ public class 计算右侧小于当前元素的个数 {
      * 合并数组
      * [left,middle)和[middle,right)
      *
-     * @param lo   最低点
+     * @param lo  最低点
      * @param mid 中点
      * @param hi  最高点（不包含）
      */
-    private void merge(final Pair[] nums, int lo, int mid, int hi) {
+    private void merge(final int[][] nums, int lo, int mid, int hi) {
 //        拷贝范围数组元素
         if (hi - lo >= 0) System.arraycopy(nums, lo, temp, lo, hi - lo);
 
@@ -61,12 +51,13 @@ public class 计算右侧小于当前元素的个数 {
                 nums[index] = temp[j++];
             } else if (j == hi) {
                 nums[index] = temp[i++];
-                result[nums[index].id] += j - mid;
-            } else if (temp[i].val < temp[j].val) {
-                nums[index] = temp[i++];
-                result[nums[index].id] += j - mid;
-            } else {
+                result[nums[index][1]] += j - mid;
+//                注意后等号添加的位置
+            } else if (temp[j][0] < temp[i][0]) {
                 nums[index] = temp[j++];
+            } else if (temp[j][0] >= temp[i][0]) {
+                nums[index] = temp[i++];
+                result[nums[index][1]] += j - mid;
             }
         }
 
@@ -74,6 +65,6 @@ public class 计算右侧小于当前元素的个数 {
 
 
     public static void main(String[] args) {
-        System.out.println(new 计算右侧小于当前元素的个数().countSmaller(new int[]{-1,-1}));
+        System.out.println(new 计算右侧小于当前元素的个数().countSmaller(new int[]{-1, -1}));
     }
 }
