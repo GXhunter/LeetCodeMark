f689ff56b6231b187393934dcb85f79632870a74
hunter <gx215145411@gmail.com>
2022-07-18 19:50:30 +0800

    除自身以外数组的乘积.java

--- src/array/除自身以外数组的乘积.java
+++ src/array/除自身以外数组的乘积.java
@@ -12,13 +12,11 @@ public class 除自身以外数组的乘积 {
 //        (i,length]的累乘
         int[] right = new int[n];
         left[0] = 1;
+        right[n - 1] = 1;
         for (int i = 1; i < n; i++) {
             left[i] = left[i - 1] * nums[i - 1];
-        }
-
-        right[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * nums[i + 1];
+            //        [1,n-1]-->[n-2,0]
+            right[n - i - 1] = right[n - i] * nums[n - i];
         }
 
         int[] res = new int[n];
