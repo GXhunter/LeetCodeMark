c5ae416a846dfc04a15310b98d532f57c6b4e3bb
wanggx <wangguoxiang@4399inc.com>
2022-07-19 09:59:00 +0800

    完全二叉树节点计算优化

--- src/tree/完全二叉树的节点个数.java
+++ src/tree/完全二叉树的节点个数.java
@@ -2,13 +2,28 @@ package tree;
 
 public class 完全二叉树的节点个数 {
     public int countNodes(TreeNode root) {
-        int count = 0;
         if (root == null) {
             return 0;
         }
-        count++;
-        count += countNodes(root.left);
-        count += countNodes(root.right);
-        return count;
+        int height = fullTreeHeight(root);
+        if (height != 0) {
+            return (int) Math.pow(2, height) - 1;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    /**
+     * @param treeNode
+     * @return 不能满二叉树返回0，否则返回高度
+     */
+    private int fullTreeHeight(TreeNode treeNode) {
+        TreeNode left = treeNode.left, right = treeNode.right;
+        int height = 0;
+        while (left != null && right != null) {
+            height++;
+            left = left.left;
+            right = right.right;
+        }
+        return left == right ? height + 1 : 0;
     }
 }
