2683138db3d3c3a516c5aed702dbbf6dde85636d
hunter <gx215145411@gmail.com>
2022-07-28 22:56:55 +0800

    平衡的二叉树

--- src/树/平衡二叉树.java
+++ src/树/平衡二叉树.java
@@ -1,27 +1,27 @@
 package 树;
-// todo bug
+
 public class 平衡二叉树 {
 
     public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return Math.abs(height(root.left) - height(root.right)) <= 1;
+        return root == null || height(root) != -1;
     }
-
     private int height(TreeNode treeNode) {
         if (treeNode == null) {
             return 0;
         }
-        return Math.max(height(treeNode.left), height(treeNode.right)) + 1;
+        int leftHeight = height(treeNode.left);
+        int rightHeight = height(treeNode.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1,
-                new TreeNode(2,
-                        new TreeNode(3,
-                                new TreeNode(4), null
-                        ), null)
+                new TreeNode(2, new TreeNode(3,
+                        new TreeNode(4), null
+                ), null)
                 , new TreeNode(3, null, new TreeNode(3, null, new TreeNode(4))
         )
         );
